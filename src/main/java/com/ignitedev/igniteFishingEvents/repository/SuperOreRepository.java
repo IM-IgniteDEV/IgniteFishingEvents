5b66781bd8badac0602bbaecbfab40942c4b42a0
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-07-09 00:36:27 +0200

    SuperOre getByLocation

--- src/main/java/com/ignitedev/igniteFishingEvents/repository/SuperOreRepository.java
+++ src/main/java/com/ignitedev/igniteFishingEvents/repository/SuperOreRepository.java
@@ -3,6 +3,8 @@ package com.ignitedev.igniteFishingEvents.repository;
 import com.ignitedev.igniteFishingEvents.base.SuperOre;
 import lombok.Getter;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,5 +19,26 @@ public class SuperOreRepository {
 
   public void removeOre(Location location) {
     spawnedSuperOres.remove(location);
+  }
+
+  @Nullable
+  public SuperOre getByLocation(Location location) {
+    for (Map.Entry<Location, SuperOre> entry : spawnedSuperOres.entrySet()) {
+      Location loc = entry.getKey();
+      SuperOre superOre = entry.getValue();
+      World world = loc.getWorld();
+
+      if (world == null) {
+        return null;
+      }
+      if (world.equals(location.getWorld())) {
+        if (location.getBlockX() == loc.getBlockX()
+            && location.getBlockY() == loc.getBlockY()
+            && location.getBlockZ() == loc.getBlockZ()) {
+          return superOre;
+        }
+      }
+    }
+    return null;
   }
 }
