e27daa1e1b6e76ad6a553b648003c7ebe38ffe8a
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-07-08 21:59:58 +0200

    Spawning CrazyZombie Utility Class

---
+++ src/main/java/com/ignitedev/igniteFishingEvents/util/CrazyZombieUtility.java
@@ -0,0 +1,56 @@
+package com.ignitedev.igniteFishingEvents.util;
+
+import com.ignitedev.aparecium.util.RandomUtility;
+import com.ignitedev.aparecium.util.collection.RandomSelector;
+import com.ignitedev.aparecium.util.text.TextUtility;
+import com.ignitedev.igniteFishingEvents.base.FishingEvent;
+import com.ignitedev.igniteFishingEvents.config.FishingEventsConfiguration;
+import com.twodevsstudio.simplejsonconfig.interfaces.Autowired;
+import lombok.experimental.UtilityClass;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Zombie;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.util.Vector;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@UtilityClass
+public class CrazyZombieUtility {
+
+  private static final ThreadLocalRandom random = ThreadLocalRandom.current();
+
+  @Autowired private static FishingEventsConfiguration configuration;
+
+  public void spawnZombie(Location location, World world, Vector reverseVelocity) {
+
+    Zombie babyZombie = ((Zombie) world.spawnEntity(location, EntityType.ZOMBIE));
+    EntityEquipment equipment = babyZombie.getEquipment();
+    List<PotionEffect> potionEffects = configuration.getCrazyZombiePotionEffects();
+
+    babyZombie.setBaby();
+    babyZombie.setVelocity(reverseVelocity);
+    babyZombie.setCustomName(TextUtility.colorize(configuration.getCrazyZombieName()));
+    babyZombie.setCustomNameVisible(true);
+    babyZombie.setConversionTime(10000);
+    babyZombie.addPotionEffects(potionEffects);
+
+    if (equipment == null) {
+      return;
+    }
+    FishingEvent fishingEvent =
+        configuration.getFishingEvents().get(LocalDateTime.now().getDayOfWeek());
+
+    if (random.nextDouble() <= fishingEvent.getSuperOreChance()) {
+      equipment.setHelmetDropChance(0);
+      // we do not drop the helmet because we are spawning super ore
+      equipment.setHelmet(
+          new ItemStack(RandomSelector.uniform(configuration.getSuperOreMaterials()).next(random)));
+    }
+  }
+}
